808203b21b941b02ccbcaf41c60d5c4ad68a1914
Nikki-Rees <nikki-rees@outlook.com>
2020-07-20 12:54:50 +1000

    working promises Testing readme.md links

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-undefined
+Title:Test generator
+Licence: mit ![alt text](http://url/to/img.png)
+
+Table of contents:
+    1[Description](#1 Description)
+    2[Installation](#2 Installation)
+    3[Usage](#3 Usage)
+    4[Licence](#4 Licence)
+    5[Contributing](#5 Contributing)
+    6[Test](#6 Test)
+    7[Questions](#7 Questions)
+
+# 1 Description
+Generate Readme
+
+# 2 Installation
+install inquirer
+
+# 3 Usage
+na
+
+# 4 Licence
+mit
+
+# 5 Contributing
+nikki marcio
+
+# 6 Test
+npm test
+
+# 7 Questions 
+Github username: nikki-rees
+If you have any questions about this application, please email me at nikki-rees@outlook.com
--- index.js
+++ index.js
@@ -5,80 +5,77 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+
 //Prompt user for for answers
 function promptUser() {
     return inquirer.prompt([
         {
             type: "input",
-            name: "ProjectName",
+            name: "title",
             message: "Please enter your project name"
         }
         ,
         {
             type: "input",
-            name: "Description",
+            name: "description",
             message: "Please enter a description for this project"
         }
         ,
         {
             type: "input",
-            name: "Installation",
+            name: "installation",
             message: "Please enter installation information for this project"
         }
         ,
         {
             type: "input",
-            name: "Usage",
+            name: "usage",
             message: "Please enter usage information for this project"
         }
         ,
         {
             type: "input",
-            name: "Licence",
+            name: "licence",
             message: "Please enter Licence information for this project"
         }
         ,
         {
             type: "input",
-            name: "Contributing",
+            name: "contributing",
             message: "Please enter contribution information for this project"
         }
         ,
         {
             type: "input",
-            name: "Test",
+            name: "test",
             message: "Please enter test information for this project"
         }
         ,
         // check how to ask two questions withint he same input name?
         {
             type: "input",
-            name: "GithubUsername",
-            message: "Please enter your Github username?"
+            name: "githubusername",
+            message: "Please enter your Github username"
         }
         ,
         {
             type: "input",
-            name: "Email",
+            name: "email",
             message: "Please enter your email"
         }
 
     ])
-        .then(answers => {
-            console.info('Answers:', answers);
-        });
 }
 
 
 //const answers =
 
-generateREADME = (answers) => {
-    return
-    `${answers.ProjectName}
-Licence: ${answers.licence}
+function generateREADME(answers) {
+    return `Title:${answers.title}
+Licence: ${answers.licence} ![alt text](http://url/to/img.png)
 
 Table of contents:
-1[Description](#1 Description)
+    1[Description](#1 Description)
     2[Installation](#2 Installation)
     3[Usage](#3 Usage)
     4[Licence](#4 Licence)
@@ -86,45 +83,50 @@ Table of contents:
     6[Test](#6 Test)
     7[Questions](#7 Questions)
 
-#1 Description
-${answers.Description}
+# 1 Description
+${answers.description}
 
-#2 Installation
-${answers.Installation}
+# 2 Installation
+${answers.installation}
 
-#3 Usage
-${answers.Usage}
+# 3 Usage
+${answers.usage}
 
-#4 Licence
-${answers.Licence}
+# 4 Licence
+${answers.licence}
 
-#5 Contributing
-${answers.Contributing}
+# 5 Contributing
+${answers.contributing}
 
-#6 Test
-${answers.Test}
+# 6 Test
+${answers.test}
 
-#7 Questions 
-Github username: ${answers.GithubUsername}
-If you have any questions about this application, please email me at ${answers.Email}
-
+# 7 Questions 
+Github username: ${answers.githubusername}
+If you have any questions about this application, please email me at ${answers.email}
 `
 }
 
-// add an axio function to get the logo for the licence and append to the file?
-
 async function init() {
-    console.log("Hi")
+    console.log("Hi");
     try {
         const answers = await promptUser();
+        console.log(answers);
         const README = generateREADME(answers);
-
         await writeFileAsync("README.md", README);
-        console.log("Successfully wrote a README file");
-
-    } catch (err) {
+        console.log("Success!")
+    } catch {
         console.log(err);
     }
+    // const answers = promptUser().then(answers => {
+    //     console.log(answers);
+    //     const README = generateREADME(answers);
+    //     return writeFileAsync("README.md", README);
+    // }).then(o => {
+    //     console.log("Success!");
+    // }).catch(err => {
+    //     console.log(err);
+    // })
 }
 
 init();
