cb4260ae824273f014b51dc17fc0de5044532650
Nikki-Rees <nikki-rees@outlook.com>
2020-07-20 10:08:02 +1000

    Update README file boiler plate text

--- index.js
+++ index.js
@@ -4,22 +4,13 @@ const fs = require("fs");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
-// Create table of content
 
-// function: 
-// {
-//     type: "choices",
-//     name: "Table of Contents",
-//     message: "Table of conetents",
-//     choices: ["Description", "Installation", "Usage", "Licence", "Contributing", "Tests", "Questions"]
-
-// }
-
-function promptModule() {
+//Prompt user for for answers
+function promptUser() {
     return inquirer.prompt([
         {
             type: "input",
-            name: "Project Name",
+            name: "ProjectName",
             message: "Please enter your project name"
         }
         ,
@@ -62,8 +53,17 @@ function promptModule() {
         // check how to ask two questions withint he same input name?
         {
             type: "input",
-            name: "Questions",
-            message: "Please enter your username?" & "what's your email?"
+            name: "GithubUsername",
+            message: "Please enter your GitHub username?",
+            message: "Please enter your email"
+
+        }
+        ,
+        {
+            type: "input",
+            name: "Email",
+            message: "Please enter your email"
+
         }
         ,
         {
@@ -84,29 +84,56 @@ function promptModule() {
 
 }
 
-
-generateMarkdown = (answers) => {
-
+// Generate a table of contents within the markdown file
+generateREADME = (answers) => {
     return
+    `${answers.ProjectName}
+Licence: ${answers.licence}
 
-    // 1[Description](#Description),
-    //     2[Installation](#Installation),
-    //     3[Usage](#Usage),
-    //     4[Licence](#Licence),
-    //     5[Contributing](#Contributing),
-    //     6[Test](#Test),
-    //     7[Questions](#Questions)
+Table of contents:
+1[Description](#1 Description)
+    2[Installation](#2 Installation)
+    3[Usage](#3 Usage)
+    4[Licence](#4 Licence)
+    5[Contributing](#5 Contributing)
+    6[Test](#6 Test)
+    7[Questions](#7 Questions)
+
+#1 Description
+${answers.Description}
+
+#2 Installation
+${answers.Installation}
+
+#3 Usage
+${answers.Usage}
+
+#4 Licence
+${answers.Licence}
+
+#5 Contributing
+${answers.Contributing}
+
+#6 Test
+${answers.Test}
+
+#7 Questions 
+Github username: ${answers.GithubUsername}
+If you have any questions about this application, please email me at ${answers.Email}
+
+`
 }
 
+// add an axio function to get the logo for the licence and append to the file
 
 async function init() {
-    console.log("hi")
+    console.log("Hi")
     try {
-        const answers = await promptModule();
-        const md = generateMarkdown(answers)
+        const answers = await promptUser();
+        const README = generateREADME(answers)
         // change file name to be input created from input to project name prompt
         // await writeFileAsync("README.md", md);
-        await writeFileAsync(`$"Project Name".md`, md);
+        await writeFileAsync("README.md", README);
         console.log("Successfully wrote a README file");
 
     } catch (err) {
